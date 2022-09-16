a5243506a279594ccbcca5e903b3bab10a503e4b
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-09-15 23:36:18 -0300

    sign in/up done

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import UserContext from "../../contexts/UserContext";
 import HomeDisplay from "../HomeDisplay/HomeDisplay";
-
+import SignInDisplay from "../SignInDisplay/SignInDisplay";
+import SignUpDisplay from "../SignUpDisplay/SignUpDisplay";
 
 export default function App(){
     const [tasks, setTasks] = useState([]);
@@ -11,6 +12,8 @@ export default function App(){
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<HomeDisplay />} />
+                    <Route path="/sign-in" element={<SignInDisplay />} />
+                    <Route path="/sign-up" element={<SignUpDisplay />} />
                 </Routes>
 		    </BrowserRouter>
         </UserContext.Provider>
